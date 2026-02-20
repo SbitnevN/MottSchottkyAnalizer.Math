24152fe88bdb1519bd89e2daad1944fae73cca24
nikita <sbitnevn@yandex.ru>
2026-02-20 22:10:38 +0300

    Математика

---
+++ SchottkyMath.cs
@@ -0,0 +1,27 @@
+﻿namespace ElinsData;
+
+public static class SchottkyMath
+{
+    /// <summary>
+    /// Calculates angular frequency ω = 2πf.
+    /// </summary>
+    /// <param name="frequency">Frequency in Hz</param>
+    /// <returns>Angular frequency in rad/s</returns>
+    public static double AngularFrequency(double frequency)
+    {
+        return 2 * Math.PI * frequency;
+    }
+
+    /// <summary>
+    /// Calculates capacitance using the relation C = -1 / (ω · Im(Z)),
+    /// where ω is the angular frequency.
+    /// </summary>
+    /// <param name="frequency">Signal frequency in Hz.</param>
+    /// <param name="impedanceImaginary">Imaginary part of impedance (Im(Z)) in Ohms.</param>
+    /// <returns>Capacitance in Farads</returns>
+    public static double Capacitance(double frequency, double impedanceImaginary)
+    {
+        double angularFrequency = AngularFrequency(frequency);
+        return Math.Round(-1.0d / (angularFrequency * impedanceImaginary), 6);
+    }
+}
